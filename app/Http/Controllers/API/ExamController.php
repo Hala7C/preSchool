3f9cba8631c8b45490f07c39f6a08c39d70c9f8e
Israa Youef <asra23745@gmail.com>
2023-08-13 23:22:52 +0300

    exam edit

--- app/Http/Controllers/API/ExamController.php
+++ app/Http/Controllers/API/ExamController.php
@@ -12,54 +12,55 @@ use Illuminate\Validation\Rule;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\File;
+
 class ExamController extends Controller
 {
 
     public function index($id)
     {
-        $subject=Subject::findOrFail($id);
-        $subjectsExam=$subject->exams()->get();
-        if($subjectsExam->count()==0){
-            return ['data'=>[],'status'=>210];
+        $subject = Subject::findOrFail($id);
+        $subjectsExam = $subject->exams()->get();
+        if ($subjectsExam->count() == 0) {
+            return ['data' => [], 'status' => 210];
         }
-        return ['data'=>$subjectsExam,'status'=>210];
+        return ['data' => $subjectsExam, 'status' => 210];
     }
 
     public function store(Request $request)
     {
 
-        $validator=Validator::make($request->all(),[
+        $validator = Validator::make($request->all(), [
             // 'name'=>['required','unique:exams,name','string'],
-            'file'=>['required','file','mimes:pdf,xlx,csv,docx', 'max:2048', Rule::unique('exams', 'name')],
-            'status'=>['required','in:avilable,unavilable'],
-            'term'=>['required','in:s1,s2'],
-            'type'=>['required','in:first,second,final'],
-            'publish_date'=>['required'],
-            'subject_id'=>['required','exists:subject,id'],
+            'file' => ['required', 'file', 'mimes:pdf,xlx,csv,docx', 'max:2048', Rule::unique('exams', 'name')],
+            'status' => ['required', 'in:avilable,unavilable'],
+            'term' => ['required', 'in:s1,s2'],
+            'type' => ['required', 'in:first,second,final'],
+            'publish_date' => ['required'],
+            'subject_id' => ['required', 'exists:subject,id'],
         ]);
-        if($validator->fails()){
-            return response()->json($validator->errors(),400);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
         }
-        $fileName =$request->file->getClientOriginalName();
-        $validat=['name'=> $fileName];
-        $validator=Validator::make($validat,[
-            'name'=>[Rule::unique('exams', 'name')]
+        $fileName = $request->file->getClientOriginalName();
+        $validat = ['name' => $fileName];
+        $validator = Validator::make($validat, [
+            'name' => [Rule::unique('exams', 'name')]
         ]);
-        if($validator->fails()){
-            return response()->json($validator->errors(),400);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
         }
         $request->file->move(public_path('exams'), $fileName);
         $myDate =  $request->publish_date;
         $date = Carbon::createFromFormat('d/m/Y H:i', $myDate)->format('Y-m-d H:i');
-        $exam=  Exam::create([
-            'name'=>$fileName,
-            'file_path'=>'exams/' . $fileName,
-            'status'=>$request->status,
-            'term'=>$request->term,
-            'type'=>$request->type,
-            'publish_date'=>$date,
-            'subject_id'=>$request->subject_id,
-            'teacher_id'=>Auth::user()->ownerable->id
+        $exam =  Exam::create([
+            'name' => $fileName,
+            'file_path' => 'exams/' . $fileName,
+            'status' => $request->status,
+            'term' => $request->term,
+            'type' => $request->type,
+            'publish_date' => $date,
+            'subject_id' => $request->subject_id,
+            'teacher_id' => Auth::user()->ownerable->id
         ]);
         $status = 210;
         return ['data' => $exam, 'status' => $status];
@@ -67,51 +68,50 @@ class ExamController extends Controller
 
     public function show($id)
     {
-        $exam=Exam::findOrFail($id);
-        return ['data'=>$exam,'status'=>210];
+        $exam = Exam::findOrFail($id);
+        return ['data' => $exam, 'status' => 210];
     }
 
     public function update(Request $request, $id)
     {
 
-        $exam=Exam::findOrFail($id);
+        $exam = Exam::findOrFail($id);
 
         ////////////can edit if publish date not coming yet
-        $publishDay=$exam->publish_date;
+        $publishDay = $exam->publish_date;
         $published = Carbon::parse($publishDay)->format("Y-m-d");
-        $cuurent=Carbon::now()->setTimezone("GMT+3")->format("Y-m-d");
-        if( Carbon::parse($cuurent)->gt($published) ){
-                // return ['data'=>['You can\'t edit this exam anymore becuase the time out'],'status'=>210];
-                return ['data'=>[],'status'=>210];
-
+        $cuurent = Carbon::now()->setTimezone("GMT+3")->format("Y-m-d");
+        if (Carbon::parse($cuurent)->gt($published)) {
+            // return ['data'=>['You can\'t edit this exam anymore becuase the time out'],'status'=>210];
+            return ['data' => [], 'status' => 210];
         }
 
-        $validator=Validator::make($request->all(),[
-            'file'=>['sometimes','mimes:pdf,xlx,csv,docx', 'max:2048', Rule::unique('exams', 'name')->ignore($exam->id)],
-            'status'=>['required','in:avilable,unavilable'],
-            'term'=>['required','in:s1,s2'],
-            'type'=>['required','in:first,second,final'],
-            'publish_date'=>['required'],
-            'subject_id'=>['required','exists:subject,id'],
+        $validator = Validator::make($request->all(), [
+            'file' => ['sometimes', 'mimes:pdf,xlx,csv,docx', 'max:2048', Rule::unique('exams', 'name')->ignore($exam->id)],
+            'status' => ['required', 'in:avilable,unavilable'],
+            'term' => ['required', 'in:s1,s2'],
+            'type' => ['required', 'in:first,second,final'],
+            'publish_date' => ['required'],
+            'subject_id' => ['required', 'exists:subject,id'],
         ]);
-        if($validator->fails()){
-            return response()->json($validator->errors(),210);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 210);
         }
-        if($request->has('file')){
-            $fileName =$request->file->getClientOriginalName();
+        if ($request->has('file')) {
+            $fileName = $request->file->getClientOriginalName();
             $request->file->move(public_path('exams'), $fileName);
-            $exam->name=$fileName;
-            $exam->file_path='exams/' . $fileName;
+            $exam->name = $fileName;
+            $exam->file_path = 'exams/' . $fileName;
         }
 
         $myDate =  $request->publish_date;
         $date = Carbon::createFromFormat('d/m/Y H:i', $myDate)->format('Y-m-d H:i');
-        $exam->status=$request->status;
-        $exam->term=$request->term;
-        $exam->type=$request->type;
-        $exam->publish_date=$date;
-        $exam->subject_id=$request->subject_id;
-        $exam->teacher_id=Auth::user()->ownerable->id;
+        $exam->status = $request->status;
+        $exam->term = $request->term;
+        $exam->type = $request->type;
+        $exam->publish_date = $date;
+        $exam->subject_id = $request->subject_id;
+        $exam->teacher_id = Auth::user()->ownerable->id;
         $exam->save();
         $status = 210;
         return ['data' => $exam, 'status' => $status];
@@ -119,7 +119,7 @@ class ExamController extends Controller
 
     public function destroy($id)
     {
-        $exam=Exam::findOrFail($id);
+        $exam = Exam::findOrFail($id);
         if (File::exists($exam->file_path)) {
             File::delete(public_path($exam->file_path));
         }
@@ -130,38 +130,39 @@ class ExamController extends Controller
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
     /////////employee section
-    public function TodayExam(){
+    public function TodayExam()
+    {
         //file - subject - class - teacher
-        $cuurent=Carbon::now()->setTimezone("GMT+3")->format("Y-m-d");
-        $data=collect();
-        $exams=Exam::whereDate('publish_date', $cuurent)->where('status','=','avilable')->get();
-        if($exams->count()==0){
-            return ['data'=>'there are not any exam for today','status'=>210];
+        $cuurent = Carbon::now()->setTimezone("GMT+3")->format("Y-m-d");
+        $data = collect();
+        $exams = Exam::whereDate('publish_date', $cuurent)->where('status', '=', 'avilable')->get();
+        if ($exams->count() == 0) {
+            return ['data' => [], 'status' => 210];
         }
-        foreach($exams as $exam){
-            $examDate=$exam->publish_date;
+        foreach ($exams as $exam) {
+            $examDate = $exam->publish_date;
             $date = Carbon::parse($examDate)->format('H:i');
             // return $examDate;
-            $subName=$exam->subject()->get()[0]->name;
-            $subID=$exam->subject()->get()[0]->id;
-            $teacherName=$exam->teacher()->get()[0]->fullName;
-            $className=$exam->class()->get('name');
-            $classID=$exam->class()->get('id');
+            $subName = $exam->subject()->get()[0]->name;
+            $subID = $exam->subject()->get()[0]->id;
+            $teacherName = $exam->teacher()->get()[0]->fullName;
+            $className = $exam->class()->get('name');
+            $classID = $exam->class()->get('id');
             $data->push([
-            'name'=>$exam->name,
-            'file_path'=>$exam->file_path,
-            'status'=>$exam->status,
-            'term'=>$exam->term,
-            'type'=>$exam->type,
-            'publish_date'=>$date,
-            'subject_name'=>$subName,
-            'subject_id'=>$subID,
-            'teacher_id'=>$exam->teacher_id,
-            'teacher_name'=>$teacherName,
-            'class_id'=>$classID,
-            'class_name'=>$className
+                'name' => $exam->name,
+                'file_path' => $exam->file_path,
+                'status' => $exam->status,
+                'term' => $exam->term,
+                'type' => $exam->type,
+                'publish_date' => $date,
+                'subject_name' => $subName,
+                'subject_id' => $subID,
+                'teacher_id' => $exam->teacher_id,
+                'teacher_name' => $teacherName,
+                'class_id' => $classID,
+                'class_name' => $className
             ]);
-    }
-        return ['data'=>$data,'status'=>210];
+        }
+        return ['data' => $data, 'status' => 210];
     }
 }
