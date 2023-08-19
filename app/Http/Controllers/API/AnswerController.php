e2d245082c5ea79b1bd8e9237e7ac4b4a94511e5
Israa Youef <asra23745@gmail.com>
2023-08-19 11:34:46 +0300

    ans img res

--- app/Http/Controllers/API/AnswerController.php
+++ app/Http/Controllers/API/AnswerController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\API;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
@@ -10,63 +11,63 @@ use App\Models\Question;
 use Illuminate\Support\Facades\File;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
+
 class AnswerController extends Controller
 {
-    public function store(Request $request,$quesId,$symbol)
+    public function store(Request $request, $quesId, $symbol)
     {
         $validator = Validator::make($request->all(), [
-            'text'=>['required', 'max:255'],
-            'img'=>['nullable','mimes:jpg,jpeg,png', 'max:1024']
+            'text' => ['required', 'max:255'],
+            'img' => ['nullable', 'mimes:jpg,jpeg,png', 'max:1024']
         ]);
         if ($validator->fails()) {
             return response()->json($validator->errors(), 400);
         }
 
-        $path=null;
-        if(isset($request->img)){
+        $path = null;
+        if (isset($request->img)) {
             $photo =  $request->input("img");
             $newphoto = time() . $photo->getClientOriginalName();
             $photo->move('answers/img', $newphoto);
-            $path= 'answers/img/' . $newphoto;
+            $path = 'answers/img/' . $newphoto;
         }
-            $input = [
-                'text'=>$request->text,
-                'img'     =>$path,
-                'symbol'     => $symbol,
-                'question_id'     => $quesId,
-            ];
-            $answer = Answer::create($input);
+        $input = [
+            'text' => $request->text,
+            'img'     => $path,
+            'symbol'     => $symbol,
+            'question_id'     => $quesId,
+        ];
+        $answer = Answer::create($input);
     }
 
-    public function update(Request $request,$quesId,$symbol)
+    public function update(Request $request, $quesId, $symbol)
     {
-        $ques=Question::findOrFail($quesId);
-        $answer=$ques->answers()->where('symbol',$symbol)->first();
+        $ques = Question::findOrFail($quesId);
+        $answer = $ques->answers()->where('symbol', $symbol)->first();
         $validator = Validator::make($request->all(), [
-            'text'=>['required', 'max:255'],
-            'img'=>['nullable','mimes:jpg,jpeg,png', 'max:1024'],
+            'text' => ['required', 'max:255'],
+            'img' => ['nullable', 'mimes:jpg,jpeg,png', 'max:1024'],
         ]);
         if ($validator->fails()) {
             return response()->json($validator->errors(), 400);
         }
-        $path=null;
-        if(isset($request->img)){
+        // $path=null;
+        if (isset($request->img)) {
             $photo =  $request->input("img");
             $newphoto = time() . $photo->getClientOriginalName();
             $photo->move('answers/img', $newphoto);
-            $path= 'answers/img/' . $newphoto;
-            $answer->img=$path;
+            $path = 'answers/img/' . $newphoto;
+            $answer->img = $path;
         }
 
-        $answer->text=$request->text;
+        $answer->text = $request->text;
         $answer->save();
-
     }
 
 
-    public function destroy( $id)
+    public function destroy($id)
     {
-        $answer=Answer::findOrFail($id);
+        $answer = Answer::findOrFail($id);
         if (File::exists($answer->img)) {
             File::delete(public_path($answer->img));
         }
